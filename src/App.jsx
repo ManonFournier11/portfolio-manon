015003a4eab56c66fa6290c1867bc452c343b5bf
Manon Fournier <manon.fournier@epitech.eu>
2025-12-04 17:23:47 +0100

    bouton

--- src/App.jsx
+++ src/App.jsx
@@ -799,16 +799,6 @@ function App() {
               <span className="contact-extra">Permis B • Véhiculée</span>
             </div>
           </div>
-
-          <div className="contact-cta">
-            <a href="mailto:manon.fournier59@gmail.com" className="btn btn-primary btn-large">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <line x1="22" y1="2" x2="11" y2="13"/>
-                <polygon points="22 2 15 22 11 13 2 9 22 2"/>
-              </svg>
-              Envoyer un message
-            </a>
-          </div>
         </div>
       </section>
 
